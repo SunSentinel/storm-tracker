cd3c610efa8ad1532ec0e716da27086664012403
David Schutz <dschutz@sun-sentinel.com>
2024-06-27 13:33:33 -0400

    Changed zoom

--- davidtest/app.js
+++ davidtest/app.js
@@ -1,7 +1,7 @@
 // CHANGE THE ZIP FILE HERE
 // var zipfile = "data/al102023_5day_007.zip";
 
-var map = L.map('map').setView([28, -76], 4.5);
+var map = L.map('map').setView([28, -76], 6.5);
 
 // Adding Voyager Basemap
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
