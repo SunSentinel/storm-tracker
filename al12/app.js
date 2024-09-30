90f5d1bd8f1be2d55b2cfa0f62de90761a5ae9fc
David Schutz <dschutz@sun-sentinel.com>
2024-09-30 08:48:24 -0400

    Changed coordinates

--- al12/app.js
+++ al12/app.js
@@ -1,7 +1,7 @@
 // CHANGE THE ZIP FILE HERE
 // var zipfile = "data/al102023_5day_007.zip";
 
-var map = L.map('map').setView([23.3, -32.7], 4.0);
+var map = L.map('map').setView([19.1, -50.8], 4.0);
 
 // Adding Voyager Basemap
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
