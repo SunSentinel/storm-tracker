2b7f8bbd85507537ce29f61749177a5c95e9d19f
David Schutz <dschutz@sun-sentinel.com>
2026-08-25 09:29:57 -0400

    Add spaghetti models

--- davidtest/app.js
+++ davidtest/app.js
@@ -1,190 +1,250 @@
-// CHANGE THE ZIP FILE HERE
 // var zipfile = "data/al102023_5day_007.zip";
 
-var map = L.map('map').setView([28, -76], 6.5);
+var map = L.map('map').setView([27.176, -92.87], 4.5);
 
-// Adding Voyager Basemap
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
+// Adding CartoDB Positron (Light & Minimal Basemap)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 18
+    subdomains: 'abcd',
+    maxZoom: 18
 }).addTo(map);
 
-// var svg = d3.select(map.getPanes().overlayPane).append("svg"),
-//     g = svg.append("g").attr("class", "leaflet-zoom-hide");
+// Add a unique timestamp AND a fake .zip fragment to trick the shapefile parser
+var cacheBuster = "?v=" + new Date().getTime() + "#.zip";
 
 
-// Try loading layers in as different shapefiles
-// WIND WARNINGS
-var wwlinZip = "data/wwlin.zip"
+// --- 0. SPAGHETTI MODEL TRACKS (FETCH & RENDER) ---
+var spaghettiLayerGroup = L.layerGroup();
+
+fetch('./data/spaghetti.geojson?v=' + new Date().getTime())
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Spaghetti GeoJSON not found or empty.');
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        if (data && data.features && data.features.length > 0) {
+            var spaghettiGeoJson = L.geoJSON(data, {
+                style: function(feature) {
+                    return {
+                        color: feature.properties.color || '#666666',
+                        weight: 2,
+                        opacity: 0.65,
+                        dashArray: '4, 4' // Subtle dashed look for secondary models
+                    };
+                },
+                onEachFeature: function(feature, layer) {
+                    if (feature.properties && feature.properties.model) {
+                        layer.bindTooltip("Model: " + feature.properties.model, {
+                            sticky: true,
+                            direction: 'auto'
+                        });
+                    }
+                }
+            });
+            spaghettiLayerGroup.addLayer(spaghettiGeoJson);
+            spaghettiLayerGroup.addTo(map);
+        }
+    })
+    .catch(function(err) {
+        console.log("Notice: Could not load spaghetti models: " + err.message);
+    });
+
+
+// --- 1. WIND WATCHES & WARNINGS ---
+var wwlinZip = "./data/wwlin.zip" + cacheBuster;
 var wwlinShp = new L.Shapefile(wwlinZip, {
     onEachFeature: function(feature, layer) {
-        if (feature.properties) {
-            layer.bindTooltip(Object.keys(feature.properties.TCWW[0]).map(function(){
-                switch(feature.properties.TCWW) {
-                    case 'TWA' : return "Tropical storm watch, as of " + feature.properties.ADVDATE;
-                    case 'TWR' : return "Tropical storm warning, as of " + feature.properties.ADVDATE;
-                    case 'HWA' : return "Hurricane watch, as of " + feature.properties.ADVDATE;
-                    case 'HWR' : return "Hurricane warning, as of " + feature.properties.ADVDATE;
-                    case 'SWA' : return "Storm surge watch, as of " + feature.properties.ADVDATE;
-                    case 'SWR' : return "Storm surge warning, as of " + feature.properties.ADVDATE;
-                }
-            }).join("<br />"), {
-                maxHeight: 40
-            });
+        if (feature.properties && feature.properties.TCWW) {
+            var status = "";
+            switch(feature.properties.TCWW) {
+                case 'TWA': status = "Tropical storm watch"; break;
+                case 'TWR': status = "Tropical storm warning"; break;
+                case 'HWA': status = "Hurricane watch"; break;
+                case 'HWR': status = "Hurricane warning"; break;
+                case 'SWA': status = "Storm surge watch"; break;
+                case 'SWR': status = "Storm surge warning"; break;
+            }
+            if (status) {
+                layer.bindTooltip(status + ", as of " + feature.properties.ADVDATE, { maxHeight: 40 });
+            }
         }
-        // console.log(Object.keys(feature.properties.TCWW[0]))
-        console.log(feature.properties.TCWW)
     },
     style: function(feature) {
         switch (feature.properties.TCWW) {
-            case 'TWA': return {color: "#FBB631"}; //tropical storm watch
-            case 'TWR':   return {color: "#136383"}; //tropical storm warning
-            case 'HWA':   return {color: "#ee6d4a"}; //hurricane watch
-            case 'HWR':   return {color: "#D80000"}; //hurricane warning
-            case 'SWA':   return {color: "#61b8bf"}; //storm surge watch ?? CODE
-            case 'SWR':   return {color: "#0000ff"}; //storm surge warning ?? CODE
+            case 'TWA': return {color: "#FBB631"}; // tropical storm watch
+            case 'TWR': return {color: "#136383"}; // tropical storm warning
+            case 'HWA': return {color: "#ee6d4a"}; // hurricane watch
+            case 'HWR': return {color: "#D80000"}; // hurricane warning
+            case 'SWA': return {color: "#61b8bf"}; // storm surge watch 
+            case 'SWR': return {color: "#0000ff"}; // storm surge warning 
         }
     },
     stroke: true,
     weight: 4,
     opacity: 0.9
  });
-
 wwlinShp.addTo(map);
 
 
-// CONE
-var coneZip = "data/pgn.zip"
+// --- 2. CONE OF UNCERTAINTY ---
+var coneZip = "./data/pgn.zip" + cacheBuster;
 var coneShp = new L.Shapefile(coneZip, {
-style: function(feature) {
-    return {
-        opacity: 0.5,
-        fillOpacity: 0.5,
-        stroke: false,
-        color: "#3d9da4"
+    style: function(feature) {
+        return {
+            opacity: 0.5,
+            fillOpacity: 0.5,
+            stroke: false,
+            color: "#3d9da4"
+        }
     }
-}
 });
 coneShp.addTo(map);
 
-// TRACK
-var trackZip = "data/lin.zip"
+
+// --- 3. STORM TRACK LINE ---
+var trackZip = "./data/lin.zip" + cacheBuster;
 var trackShp = new L.Shapefile(trackZip, {
-style: function(feature) {
-    return {
-        opacity: 1,
-        fillOpacity: 1,
-        stroke: true,
-        weight: 1.75,
-        color: "black"
+    style: function(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            stroke: true,
+            weight: 2.5,
+            color: "black"
+        }
     }
-}
 });
 trackShp.addTo(map);
 
+// Automatically zoom and pan to the storm track once data loads
+trackShp.once("data:loaded", function() {
+    if (trackShp.getBounds().isValid()) {
+        map.fitBounds(trackShp.getBounds(), { padding: [100, 100] });
+    }
+});
 
-// POINTS
-var pointsZip = "data/points.zip"
+
+// --- 4. FORECAST POINTS (CUSTOM ALPHABETIC MARKERS) ---
+var pointsZip = "./data/points.zip" + cacheBuster;
 var pointsShp = new L.Shapefile(pointsZip, {
-onEachFeature: function(feature, layer) {
-    if (feature.properties) {
-         // PREV: layer.bindTooltip(Object.keys(feature.properties.DVLBL).map(function(k) {
-        layer.bindTooltip(Object.keys(feature.properties.DVLBL[0]).map(function() {
+    onEachFeature: function(feature, layer) {
+        if (feature.properties && feature.properties.DVLBL) {
+            var status = "";
             switch(feature.properties.DVLBL) {
-                case 'D' : return "Status: Tropical depression, winds under 39 mph <br /> Expected: " + feature.properties.DATELBL;
-                case 'S' : return "Status: Tropical storm, winds between 39 to 73 mph <br /> Expected: " + feature.properties.DATELBL;
-                case 'H' : return "Status: Hurricane, winds between 74 and 110 mph <br /> Expected: " + feature.properties.DATELBL;
-                case 'M' : return "Status: Major hurricane, winds greater than 110 mph <br /> Expected: " + feature.properties.DATELBL;
+                case 'D' : status = "Status: Tropical depression, winds under 39 mph"; break;
+                case 'S' : status = "Status: Tropical storm, winds between 39 to 73 mph"; break;
+                case 'H' : status = "Status: Hurricane, winds between 74 and 110 mph"; break;
+                case 'M' : status = "Status: Major hurricane, winds greater than 110 mph"; break;
             }
-        })
-        .join("<br />"), {
-            maxHeight: 50
+            if (status) {
+                layer.bindTooltip(status + "<br /> Expected: " + feature.properties.DATELBL, { maxHeight: 50 });
+            }
+        }
+    },
+    pointToLayer: function(feature, latlng) {
+        var letter = feature.properties.DVLBL;
+        var customClass = "storm-icon icon-" + letter;
+        
+        var stormIcon = L.divIcon({
+            className: customClass,
+            html: letter,
+            iconSize: [18, 18],
+            iconAnchor: [9, 9]
         });
-        // console.log(feature.properties)
+        
+        return L.marker(latlng, { icon: stormIcon });
     }
-},
-pointToLayer: function(feature, latlng) {
-    return L.circleMarker(latlng, {
-        opacity: 1,
-        fillOpacity: 1,
-        color: "black"
-    });
-},
-style: function(feature) {
-    switch(feature.properties.DVLBL) {
-        case 'D' : return {radius: 1};
-        case 'S' : return {radius: 2};
-        case 'H' : return {radius: 3};
-        case 'M' : return {radius: 4, color: "red"};
-    }
-}
 });
 pointsShp.addTo(map).bringToFront();
 
 
+// --- 5. NATIVE COLLAPSIBLE LEGEND ---
+var legend = L.control({ position: 'bottomright' });
 
- // LEGEND
-var legend = L.control.legend({
-    position: 'bottomright',
-    collapsed: false,
-    symbolWidth: 16,
-    opacity: 0.9,
-    column: 2,
-    legends: [{
-        label: "Storm path",
-        type: "circle",
-        fillColor: "#3d9da4",
-        opacity: 0.5,
-        radius: 6
-    },
-    {
-        label: "Tropical storm, winds 39 to 73 mph",
-        type: "circle",
-        color: "black",
-        fillColor: "black",
-        radius: 2
-    },
-    {
-        label: "Hurricane, winds 74 to 110 mph",
-        type: "circle",
-        color: "black",
-        fillColor: "black",
-        radius: 3
-    },
-    {
-        label: "Major hurricane, winds over 110 mph",
-        type: "circle",
-        color: "red",
-        fillColor: "red",
-        radius: 4
-    },
-    {
-        label: "Tropical storm watch",
-        type: "polyline",
-        color: "#FBB631",
-        weight: 4,
-    },
-    {
-        label: "Tropical storm warning",
-        type: "polyline",
-        color: "#136383",
-        weight: 4,
-    },
-    {
-        label: "Hurricane watch",
-        type: "polyline",
-        color: "#ee6d4a",
-        weight: 4,
-    },
-    {
-        label: "Hurricane warning",
-        type: "polyline",
-        color: "#d80000",
-        weight: 4,
-    }]
-})
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'leaflet-legend leaflet-bar leaflet-control');
+    div.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    div.style.padding = '10px';
+    div.style.lineHeight = '1.8em';
+    div.style.color = '#333';
+    
+    L.DomEvent.disableClickPropagation(div);
+
+    var header = L.DomUtil.create('div', 'legend-header', div);
+    header.style.cursor = 'pointer';
+    header.style.textAlign = 'center';
+    
+    var content = L.DomUtil.create('div', 'legend-content', div);
+
+    var html = '';
+    // Storm Points
+    html += '<div style="text-align: left; margin-top: 5px;">';
+    html += '<strong>Storm Status</strong><br>';
+    html += '<div class="storm-icon icon-D" style="display:inline-block; width:18px; height:18px; margin-right:6px; vertical-align:middle;">D</div> Tropical depression<br>';
+    html += '<div class="storm-icon icon-S" style="display:inline-block; width:18px; height:18px; margin-right:6px; vertical-align:middle;">S</div> Tropical storm<br>';
+    html += '<div class="storm-icon icon-H" style="display:inline-block; width:18px; height:18px; margin-right:6px; vertical-align:middle;">H</div> Hurricane<br>';
+    html += '<div class="storm-icon icon-M" style="display:inline-block; width:18px; height:18px; margin-right:6px; vertical-align:middle;">M</div> Major hurricane<br>';
+
+    // Watches & Warnings
+    html += '<br><strong>Watches & Warnings</strong><br>';
+    html += '<div style="display:inline-block; width:18px; height:4px; background-color:#FBB631; margin-right:6px; vertical-align:middle;"></div> Tropical storm watch<br>';
+    html += '<div style="display:inline-block; width:18px; height:4px; background-color:#136383; margin-right:6px; vertical-align:middle;"></div> Tropical storm warning<br>';
+    html += '<div style="display:inline-block; width:18px; height:4px; background-color:#ee6d4a; margin-right:6px; vertical-align:middle;"></div> Hurricane watch<br>';
+    html += '<div style="display:inline-block; width:18px; height:4px; background-color:#d80000; margin-right:6px; vertical-align:middle;"></div> Hurricane warning<br>';
+
+    // Path & Cone
+    html += '<br><strong>Official Forecast</strong><br>';
+    html += '<div style="display:inline-block; width:18px; height:18px; background-color:#3d9da4; opacity:0.5; border-radius:50%; margin-right:6px; vertical-align:middle;"></div> Forecast cone<br>';
+    html += '<div style="display:inline-block; width:18px; height:3px; background-color:black; margin-right:6px; vertical-align:middle;"></div> Official track<br>';
+
+    // Spaghetti Models Section & Toggle
+    html += '<br><strong>Computer Models</strong><br>';
+    html += '<label style="cursor:pointer; user-select:none;"><input type="checkbox" id="toggleSpaghetti" checked style="vertical-align:middle; margin-right:5px;"> Show spaghetti lines</label><br>';
+    html += '<div style="display:inline-block; width:14px; height:0px; border-top:2px dashed #e41a1c; margin-right:4px; vertical-align:middle;"></div> GFS ';
+    html += '<div style="display:inline-block; width:14px; height:0px; border-top:2px dashed #377eb8; margin-right:4px; margin-left:6px; vertical-align:middle;"></div> Euro (ECMWF)<br>';
+    html += '<div style="display:inline-block; width:14px; height:0px; border-top:2px dashed #984ea3; margin-right:4px; vertical-align:middle;"></div> HWRF ';
+    html += '<div style="display:inline-block; width:14px; height:0px; border-top:2px dashed #4daf4a; margin-right:4px; margin-left:6px; vertical-align:middle;"></div> HMON<br>';
+
+    html += '</div>';
+
+    content.innerHTML = html;
+
+    // Toggle logic function
+    header.onclick = function() {
+        if (content.style.display === 'none') {
+            content.style.display = 'block';
+            header.innerHTML = '<strong>Legend &#9660;</strong>';
+        } else {
+            content.style.display = 'none';
+            header.innerHTML = '<strong>Legend &#9650;</strong>';
+        }
+    };
+
+    if (window.innerWidth <= 450) {
+        content.style.display = 'none';
+        header.innerHTML = '<strong>Legend &#9650;</strong>'; 
+    } else {
+        content.style.display = 'block';
+        header.innerHTML = '<strong>Legend &#9660;</strong>'; 
+    }
+
+    return div;
+};
+
 legend.addTo(map);
 
-
-
+// Add event listener for the Spaghetti checkbox toggle after legend mounts
+setTimeout(function() {
+    var toggle = document.getElementById('toggleSpaghetti');
+    if (toggle) {
+        toggle.addEventListener('change', function(e) {
+            if (e.target.checked) {
+                map.addLayer(spaghettiLayerGroup);
+            } else {
+                map.removeLayer(spaghettiLayerGroup);
+            }
+        });
+    }
+}, 300);
--- davidtest/scraper.py
+++ davidtest/scraper.py
@@ -1,74 +1,163 @@
-import requests, zipfile, io
-from bs4 import BeautifulSoup
-from zipfile import ZipFile
-import glob
 import os
+import io
+import glob
+import json
+import shutil
+import zipfile
+import requests
+from bs4 import BeautifulSoup
 
-# import wget
+# --- 1. BULLETPROOF DIRECTORY SETUP ---
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(SCRIPT_DIR, 'data')
 
-## FOR A NEW STORM FILE, YOU NEED TO CHANGE LINES 10 AND 11
-stormname = 'davidtest'
-url = "https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id=al01&year=2024"
+# ---------------------------------------------------
+# CHANGE THESE VARIABLES FOR THE ACTIVE STORM
+stormname = 'al03' 
+year = '2026'
+# ---------------------------------------------------
+
+url = f"https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id={stormname}&year={year}"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
+# 2. Get ONLY the zip files that have '5day' in their name
+anchors = soup.find_all(lambda tag: tag.name=='a' and '5day' in tag.text and tag.text.endswith('.zip'))
 
-anchor = soup.find_all(lambda tag: tag.name=='a' and tag.text.endswith('.zip'))
-link = anchor[-1].get('href')
-folder = anchor[-1].get_text(strip=True)
+# 3. Filter out intermediate advisories (e.g., 001A.zip)
+full_advisories = [a for a in anchors if a.text.replace('.zip', '')[-1].isdigit()]
 
-# link = a['href']
-# folder = a.get_text(strip=True)
+if not full_advisories:
+    print(f"Error: No full 5-day advisories found for {stormname.upper()} {year}.")
+    exit()
+
+# 4. Grab the latest FULL 5-day advisory
+link = full_advisories[-1].get('href')
 
 landing = "https://www.nhc.noaa.gov/gis/"
-final = landing+link
-# wget.download(final)
+final = landing + link
 
-## CLEAR THE EXISTING ZIP FILES
-for olddata in glob.glob(stormname + '/data/*', recursive=True):
-    os.remove(olddata)
+# --- 5. FRESH DATA FOLDER ---
+if os.path.exists(DATA_DIR):
+    shutil.rmtree(DATA_DIR)
 
+os.makedirs(DATA_DIR, exist_ok=True)
+
+# --- 6. DOWNLOAD AND EXTRACT NHC SHAPEFILES ---
+print(f"Downloading NHC data from: {final}")
 r = requests.get(final)
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall("./" + stormname + "/data")
+z.extractall(DATA_DIR)
 
-#    for file in glob.glob('folder[0:8] + "-" + folder[14:17] + "_5day_pts'):
+# --- 7. REPACKAGE ZIPS ---
+# Points
+with zipfile.ZipFile(os.path.join(DATA_DIR, 'points.zip'), 'w') as zipF:
+    for file in glob.glob(f"{DATA_DIR}/**/*5day_pts*", recursive=True):
+        zipF.write(file, arcname=os.path.relpath(file, DATA_DIR))
 
-# with zipfile.ZipFile(stormname + '/data/pts.zip', 'w') as zipF:
-#     for file in glob.glob(stormname + '/data/*5day_pts*', recursive=True):
-#         zipF.write(file)
+# Cone
+with zipfile.ZipFile(os.path.join(DATA_DIR, 'pgn.zip'), 'w') as zipF:
+    for file in glob.glob(f"{DATA_DIR}/**/*5day_pgn*", recursive=True):
+        zipF.write(file, arcname=os.path.relpath(file, DATA_DIR))
 
-# Parse into 4 separate Zip files: line, pgn, points, wwlin
-# For the time points
-with zipfile.ZipFile(stormname + '/data/points.zip', 'w') as zipF:
-    for file in glob.glob(stormname + '/data/*5day_pts*', recursive=True):
-        full_path = file
-        relative_path = stormname + '/data'
-        zipF.write(file, arcname=os.path.relpath(full_path, relative_path))
+# Track
+with zipfile.ZipFile(os.path.join(DATA_DIR, 'lin.zip'), 'w') as zipF:
+    for file in glob.glob(f"{DATA_DIR}/**/*5day_lin*", recursive=True):
+        zipF.write(file, arcname=os.path.relpath(file, DATA_DIR))
 
+# Wind warnings
+with zipfile.ZipFile(os.path.join(DATA_DIR, 'wwlin.zip'), 'w') as zipF:
+    for file in glob.glob(f"{DATA_DIR}/**/*wwlin*", recursive=True):
+        if not file.endswith('.zip'):
+            zipF.write(file, arcname=os.path.relpath(file, DATA_DIR))
 
-# For the cone of uncertainty
-with zipfile.ZipFile(stormname + '/data/pgn.zip', 'w') as zipF:
-    for file in glob.glob(stormname + '/data/*5day_pgn*', recursive=True):
-        full_path = file
-        relative_path = stormname + '/data'
-        zipF.write(file, arcname=os.path.relpath(full_path, relative_path))
+# --- 8. FETCH & CONVERT SPAGHETTI MODEL DATA (ATCF -> GeoJSON) ---
+print(f"Fetching spaghetti model guidance for {stormname.upper()}...")
 
-# For the track
-with zipfile.ZipFile(stormname + '/data/lin.zip', 'w') as zipF:
-    for file in glob.glob(stormname + '/data/*5day_lin*', recursive=True):
-        full_path = file
-        relative_path = stormname + '/data'
-        zipF.write(file, arcname=os.path.relpath(full_path, relative_path))
+# Target major forecast models (GFS, ECMWF, HWRF, HMON, NVGM/Navgem, ICON, UKMET)
+TRACKED_MODELS = {'AVNO': 'GFS', 'EMX': 'ECMWF', 'HMNI': 'HMON', 'HWFI': 'HWRF', 'NVGM': 'NAVGEM', 'ICON': 'ICON', 'UKM': 'UKMET'}
+MODEL_COLORS = {'GFS': '#e41a1c', 'ECMWF': '#377eb8', 'HMON': '#4daf4a', 'HWRF': '#984ea3', 'NAVGEM': '#ff7f00', 'ICON': '#ffff33', 'UKMET': '#a65628'}
 
-# For wind/hurricane advisories
-with zipfile.ZipFile(stormname + '/data/wwlin.zip', 'w') as zipF:
-    for file in glob.glob(stormname + '/data/*5day_wwlin*', recursive=True):
-        full_path = file
-        relative_path = stormname + '/data'
-        zipF.write(file, arcname=os.path.relpath(full_path, relative_path))
+# UCAR/RAL real-time ATCF data URL structure
+atcf_url = f"https://www.ral.ucar.edu/hurricanes/realtime/plots/northatlantic/{year}/{stormname.lower()}/a{stormname.lower()}{year}.dat"
 
+try:
+    atcf_resp = requests.get(atcf_url, timeout=10)
+    features = []
 
+    if atcf_resp.status_code == 200:
+        model_tracks = {}
+        lines = atcf_resp.text.strip().split('\n')
 
+        for line in lines:
+            parts = [p.strip() for p in line.split(',')]
+            if len(parts) >= 11:
+                technique = parts[4]
+                if technique in TRACKED_MODELS:
+                    model_label = TRACKED_MODELS[technique]
+                    lat_raw = parts[6]
+                    lon_raw = parts[7]
 
+                    # Parse Latitude (e.g., 257N -> 25.7)
+                    lat = float(lat_raw[:-1]) / 10.0
+                    if lat_raw.endswith('S'):
+                        lat = -lat
 
+                    # Parse Longitude (e.g., 801W -> -80.1)
+                    lon = float(lon_raw[:-1]) / 10.0
+                    if lon_raw.endswith('W'):
+                        lon = -lon
+
+                    if model_label not in model_tracks:
+                        model_tracks[model_label] = []
+                    
+                    # Avoid duplicate sequential coordinates
+                    coord = [lon, lat]
+                    if not model_tracks[model_label] or model_tracks[model_label][-1] != coord:
+                        model_tracks[model_label].append(coord)
+
+        # Build GeoJSON Features
+        for model_label, coords in model_tracks.items():
+            if len(coords) > 1:
+                features.append({
+                    "type": "Feature",
+                    "properties": {
+                        "model": model_label,
+                        "color": MODEL_COLORS.get(model_label, '#888888')
+                    },
+                    "geometry": {
+                        "type": "LineString",
+                        "coordinates": coords
+                    }
+                })
+
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    # Save spaghetti.geojson to data/
+    spaghetti_path = os.path.join(DATA_DIR, 'spaghetti.geojson')
+    with open(spaghetti_path, 'w') as f:
+        json.dump(geojson_data, f, indent=2)
+
+    print(f"Generated spaghetti.geojson with {len(features)} model tracks!")
+
+except Exception as e:
+    print(f"Warning: Could not process spaghetti model data ({e}). Creating empty file.")
+    spaghetti_path = os.path.join(DATA_DIR, 'spaghetti.geojson')
+    with open(spaghetti_path, 'w') as f:
+        json.dump({"type": "FeatureCollection", "features": []}, f)
+
+# --- 9. CLEANUP RAW SHAPEFILES ---
+allowed_files = ['points.zip', 'pgn.zip', 'lin.zip', 'wwlin.zip', 'spaghetti.geojson']
+
+for item in os.listdir(DATA_DIR):
+    if item not in allowed_files:
+        item_path = os.path.join(DATA_DIR, item)
+        if os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+        else:
+            os.remove(item_path)
+
+print(f"Successfully processed and saved all test data for {stormname.upper()} into {DATA_DIR}!")
